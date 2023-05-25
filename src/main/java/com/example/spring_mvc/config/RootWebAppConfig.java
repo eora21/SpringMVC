213333c03139bd28082faa5d6b2c71c16f4fbcd8
eora21 <eora21@naver.com>
2023-05-25 21:35:24 +0900

    feat: 🎸 변경 컴포넌트 스캔 범위 모든 프로젝트로

--- src/main/java/com/example/spring_mvc/config/RootWebAppConfig.java
+++ src/main/java/com/example/spring_mvc/config/RootWebAppConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Controller;
 
 @Configuration
-@ComponentScan(basePackages = "com.example.spring_mvc.domain", excludeFilters = @ComponentScan.Filter(Controller.class))
+@ComponentScan(basePackages = "com.example.spring_mvc", excludeFilters = @ComponentScan.Filter(Controller.class))
 public class RootWebAppConfig {
 
 }
